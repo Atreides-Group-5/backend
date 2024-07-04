a0495d84c6747f0a14d4edc3ddd07cf459a21206
Pat <patb.software@gmail.com>
2024-07-04 21:26:35 +0700

    Add createTrip api route

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -4,7 +4,7 @@ import Trip from '../models/tripModel.js';
 const getAllTrips = async (req, res, next) => {
     try {
         const trips = await Trip.find();
-        res.status(200).json({ message: 'Get All Movies', trips: trips });
+        res.status(200).json({ message: 'Get All Trips', trips: trips });
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
@@ -19,4 +19,38 @@ const getTripById = async (req, res, next) => {
     }
 }
 
-export { getAllTrips, getTripById };
+const createTrip = async (req, res, next) => {
+    try {
+        const {
+            name,
+            start_date,
+            end_date,
+            destination_from,
+            destination_to,
+            rating,
+            price,
+            description,
+            sub_expenses,
+            images
+        } = req.body;
+        const tripData = {
+            name,
+            start_date,
+            end_date,
+            destination_from,
+            destination_to,
+            rating,
+            price,
+            description,
+            sub_expenses,
+            images
+        };
+        // VALIDATE tripData done by mongoose
+        const trip = await Trip.create(tripData);
+        res.status(201).json({ message: 'Created Trip', data: trip });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export { getAllTrips, getTripById, createTrip };
