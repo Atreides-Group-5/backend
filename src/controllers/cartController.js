0d9acbd2801d4d9286f82afa52413c602b045fa7
Pat <patb.software@gmail.com>
2024-07-09 16:17:00 +0700

    Add delete and get routes for cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -22,12 +22,46 @@ const addToCart = async (req, res) => {
             });
         }
         await cart.save();
-        res.json({ message: 'Successfully added to cart',  });
+        res.json({ message: 'Successfully added to cart' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Failed to add to cart' });
     }
 }
 
+// Get user cart
+const getCart = async (req, res) => {
+    try {
+        let cart = await Cart.findOne({ user_id: req.user.id });
+        if (!cart) {
+            cart = new Cart({
+                user_id: req.user.id,
+                cart: []
+            });
+            cart = await Cart.create(cart);
+        }
+        
+        console.log(cart);
+        res.json({ message: 'Successfully retrieved cart', cart: cart.cart });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to retrieve cart' });
+    }
+}
 
-export { addToCart, getCart };
+// Delete from cart
+const deleteFromCart = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Cart.findOneAndUpdate(
+            { user_id: req.user.id },
+            { $pull: { cart: { _id: id } } }
+        );
+        res.json({ message: 'Successfully deleted from cart' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to delete from cart' });
+    }
+}
+
+export { addToCart, getCart, deleteFromCart };
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -5,5 +5,7 @@ import { authenticateMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post('/', authenticateMiddleware, cartController.addToCart);
+router.get('/', authenticateMiddleware, cartController.getCart);
+router.delete('/:id', authenticateMiddleware, cartController.deleteFromCart);
 
 export default router;
