66eb6b2f692dbd057b70fb66921b8aed9b3011a9
Smile.nr <nattakit.rattanakeha@gmail.com>
2024-07-12 16:03:08 +0700

    add cors origin

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
@@ -13,26 +13,26 @@ import userProfileRoutes from "./routes/userProfileRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 
-import errorMiddleware from './middlewares/errorMiddleware.js'; 
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 // Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log('MONGO_URI:', process.env.MONGO_URI);  // Debug
-console.log('PORT:', process.env.PORT);  // Debug
+console.log("MONGO_URI:", process.env.MONGO_URI); // Debug
+console.log("PORT:", process.env.PORT); // Debug
 
 const app = express();
 
 // Define CORS options
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    optionsSuccessStatus: 200
+  origin: "voyage-frontend-chi.vercel.app",
+  optionsSuccessStatus: 200,
 };
 
 // Enable CORS with options
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '4mb' }));
+app.use(express.json({ limit: "4mb" }));
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // ไม่ต้องใช้แล้ว
 
 // Routes
@@ -45,7 +45,7 @@ app.use("/api/bookings", bookingRoutes);
 
 // Handle 404 route not found
 app.use((req, res) => {
-    res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 // Handle Errors
 app.use(errorMiddleware);
@@ -55,17 +55,18 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
-    console.error("MongoDB URI is not defined in environment variables");
-    process.exit(1);
+  console.error("MongoDB URI is not defined in environment variables");
+  process.exit(1);
 }
 
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error("MongoDB connection error:", error);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
