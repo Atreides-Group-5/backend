7a0e74105c587003b0ec500691084484db51e843
Smile.nr <nattakit.rattanakeha@gmail.com>
2024-07-13 16:42:50 +0700

    add get booking by id

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -15,9 +15,24 @@ const router = express.Router();
 router.get("/", getAllTrips);
 router.get("/search", getTripsByName);
 router.get("/:id", getTripById);
-router.post("/", authenticateAdminMiddleware, upload.array("images"), createTrip);
-router.put("/:id", authenticateAdminMiddleware, upload.array("images"), updateTrip);
-router.patch("/:id", authenticateAdminMiddleware, upload.array("images"), updateTrip);
+router.post(
+  "/",
+  authenticateAdminMiddleware,
+  upload.array("images"),
+  createTrip
+);
+router.put(
+  "/:id",
+  authenticateAdminMiddleware,
+  upload.array("images"),
+  updateTrip
+);
+router.patch(
+  "/:id",
+  authenticateAdminMiddleware,
+  upload.array("images"),
+  updateTrip
+);
 router.delete("/:id", authenticateAdminMiddleware, deleteTrip);
 
 export default router;
